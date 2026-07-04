9598852c35b4383dbb27fe54e32282e3b8f50323
Nadim Nahle <nahlenadim@gmail.com>
2026-07-04 11:52:54 +0300

    Add register to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { LoginPage } from "./pages/LoginPage";
 import { DashboardPage } from "./pages/DashboardPage";
 import { ProtectedRoute } from "./components/ProtectedRoute";
+import { RegisterPage } from "./pages/RegisterPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
 
       <Route
         path="/dashboard"
